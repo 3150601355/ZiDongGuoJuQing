f2cefef34239346128835d6cace901bf3bbe80e4
偶尔有点小迷糊 <29688710+3150601355@users.noreply.github.com>
2023-11-11 23:40:23 +0800

    使用灰度判定同时增加了按键控制开关 'n'

--- k快速跳过剧情3.py
+++ k快速跳过剧情3.py
@@ -6,16 +6,36 @@ import numpy                            # pip install numpy
 from PIL import ImageGrab               # pip install pillow
 import sys, time, ctypes                # python自带 不用安装
 from random import random               # python自带 不用安装
+import keyboard                         #pip install keyboard
+import threading
 
-img_templ = cv2.imread('play_button.jpg' ) #加载模板图片
-THRESHOLD = 0.97
+img_templ = cv2.imread('play_button.jpg', 0)#换为灰度
+_,img_templ = cv2.threshold(img_templ, 127, 255, cv2.THRESH_BINARY)#thresh 参数为灰度阈值
+
+# cv2.imshow('Template Image', img_templ) #观察图片
+# cv2.waitKey(0)  # 等待按键后关闭窗口
+# cv2.destroyAllWindows()
+# sys.exit()
+THRESHOLD = 0.85
+open_switch = True
+def listen_for_keypress():
+    global open_switch
+    while True:
+        time.sleep(0.09)
+        if keyboard.is_pressed('n'):  # 使用keyboard库检查是否有键盘事件
+            open_switch = not open_switch
+            print('开关按下')
 
 
 def mainLoop():
     #截图
     img_src = ImageGrab.grab( bbox=(80, 45, 110, 85) ) # x1, y1, x2, y2
+
     # img_src.save("capture.jpg") # for debug
     img_src = cv2.cvtColor(numpy.asarray(img_src), cv2.COLOR_RGB2BGR)
+    img_src = cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY)  # 转换为灰度图像
+    _, img_src = cv2.threshold(img_src, 127, 255, cv2.THRESH_BINARY)  # thresh 参数为灰度阈值
+
 
     #模板匹配
     result = cv2.matchTemplate(img_src, img_templ, cv2.TM_CCOEFF_NORMED)
@@ -29,13 +49,19 @@ def mainLoop():
         time.sleep(0.05 + 0.1 * random())
         ctypes.windll.user32.mouse_event(4)
 
-    
+
 if __name__ == '__main__':
+
     # 判断当前进程是否以管理员权限运行
     if ctypes.windll.shell32.IsUserAnAdmin() :
         print('当前已是管理员权限')
+
+        # 启动键盘监听线程
+        listener_thread = threading.Thread(target=listen_for_keypress)
+        listener_thread.start()
         while True:
-            mainLoop()
+            if open_switch:
+                mainLoop()
             time.sleep(0.3 + 0.2 * random())
     else:
         print('当前不是管理员权限，以管理员权限启动新进程...')
